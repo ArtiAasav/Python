b61a54dc8f121b9240b7f7c9449fc6b076fe1d30
ArtiAasav <arti@hkhk.edu.ee>
2024-10-04 15:38:49 +0300

    Add files via upload

---
+++ 01.1.py
@@ -0,0 +1,49 @@
+import turtle
+# Arti Aasav
+# 04.10.2024
+# Harjutus 01 Kilpkonn
+
+#muudan asukohta
+turtle.penup()
+turtle.goto(-400,200)
+turtle.pendown()
+#tegin kolnurga
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+
+#muudan asukohta
+turtle.penup()
+turtle.goto(200,100)
+turtle.pendown()
+#joonistan oma eesnime esitähe
+turtle.fd(100)
+turtle.rt(70)
+turtle.fd(50)
+turtle.lt(70)
+turtle.fd(50)
+turtle.lt(110)
+turtle.fd(275)
+turtle.lt(70)
+turtle.fd(45)
+turtle.lt(70)
+turtle.fd(275)
+turtle.lt(110)
+turtle.fd(50)
+turtle.lt(70)
+turtle.fd(50)
+turtle.penup()
+turtle.fd(50)
+turtle.pendown()
+turtle.fd(100)
+turtle.rt(140)
+turtle.fd(100)
+turtle.rt(110)
+turtle.fd(75)
+
+
+#Lõpetan, et ei jookseks kokku
+turtle.done()
