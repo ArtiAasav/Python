181f70733046d36f69547af8e77798a48af699e6
ArtiAasav <arti@hkhk.edu.ee>
2024-11-01 11:24:55 +0200

    Update 09.py

--- 09.py
+++ 09.py
@@ -54,99 +54,138 @@ import random
 
 
 
-# Kuva nimekirjast unikaalsed nimed:
-nimed = ['Martin', 'Tõnu', 'Andres', 'Tõnu', 'Andres', 'Andres', 'Andres', 'Tõnu', 'Marko', 'Mari', 'Jüri', 'Liis', 'Marko', 'Piret', 'Anu']
-unikaalsed_nimed = []
-for nimi in nimed:
-    if nimi not in unikaalsed_nimed:
-        unikaalsed_nimed.append(nimi)
+# # Kuva nimekirjast unikaalsed nimed:
+# nimed = ['Martin', 'Tõnu', 'Andres', 'Tõnu', 'Andres', 'Andres', 'Andres', 'Tõnu', 'Marko', 'Mari', 'Jüri', 'Liis', 'Marko', 'Piret', 'Anu']
+# unikaalsed_nimed = []
+# for nimi in nimed:
+#     if nimi not in unikaalsed_nimed:
+#         unikaalsed_nimed.append(nimi)
 
-for nimi in unikaalsed_nimed:
-    print(nimi)
-
-
+# for nimi in unikaalsed_nimed:
+#     print(nimi)
 
 
-# Sulle on saadetud õpilaste keskmised hinded, mille lisasid loendisse. Eralda hinded ning leia kogu rühma parim ja kehvem tulemus ning keskmine hinne.
-ryhma_hinded = ["Mari 4.9", "Jüri 3.1", "Kadri 4.6", "Marko 4.7", "Liis 4.9", "Andres 4.2", "Anu 4.7", "Martin 4.2", "Piret 4.2", "Tõnu 4.1"]
-hinded = []
 
-for i in ryhma_hinded:
-    hinded.append(float(i.split(" ")[1]))
 
-print(f"Parim tulemus {max(hinded)}")
-print(f"Halvim tulemus {min(hinded)}")
-print(f"Keskmine tulemus {sum(hinded)/len(hinded)}")
+# # Sulle on saadetud õpilaste keskmised hinded, mille lisasid loendisse. Eralda hinded ning leia kogu rühma parim ja kehvem tulemus ning keskmine hinne.
+# ryhma_hinded = ["Mari 4.9", "Jüri 3.1", "Kadri 4.6", "Marko 4.7", "Liis 4.9", "Andres 4.2", "Anu 4.7", "Martin 4.2", "Piret 4.2", "Tõnu 4.1"]
+# hinded = []
 
-# Koosta programm, mis genereerib ja kuvab korrutustabeli, kus iga number korrutatakse iseendaga:
-# Näiteks:
-# 0 x 0 = 0
-# 1 x 1 = 1
-# 2 x 2 = 4
+# for i in ryhma_hinded:
+#     hinded.append(float(i.split(" ")[1]))
 
-for i in range(11):
-    print(f"{i} x {i} = {i*1}")
+# print(f"Parim tulemus {max(hinded)}")
+# print(f"Halvim tulemus {min(hinded)}")
+# print(f"Keskmine tulemus {sum(hinded)/len(hinded)}")
 
-# Loo programm, mis loob suvalised tehted 1-100 arvudega.
+# # Koosta programm, mis genereerib ja kuvab korrutustabeli, kus iga number korrutatakse iseendaga:
+# # Näiteks:
+# # 0 x 0 = 0
+# # 1 x 1 = 1
+# # 2 x 2 = 4
 
-# Kasuta tsükli puhul alakriipsu
-# kasuta suvalise tehte märgi jaoks loendit ja sealt suvalise märgi leidmiseks random.choice()
-# Näiteks:
-# 7 – 2=
-# 45 * 69=
+# for i in range(11):
+#     print(f"{i} x {i} = {i*1}")
 
-kysimustearv= 5
-punktid = 0
-tehted = ['+','-','*','/']
-for i in range(kysimustearv+1):
-    i = random.randint(1,10)
-    j = random.randint(1,10)
-    tehe = random.choice(tehted)
-    if tehe=="+":
-        print(f"{i} {tehe} {j} = ")
-        vastus = int(input("Vastus: "))
-        if vastus==i+j:
-            print("Õige")
-            punktid+=1
-        else:
-            print("Vale vastus")
-    elif tehe=="-":
-        print(f"{i} {tehe} {j} = ")
-        vastus = int(input("Vastus: "))
-        if vastus==i-j:
-            print("Õige")
-            punktid+=1
-        else:
-            print("Vale vastus")
-    elif tehe=="*":
-        print(f"{i} {tehe} {j} = ")
-        vastus = int(input("Vastus: "))
-        if vastus==i*j:
-            print("Õige")
-            punktid+=1
-        else:
-            print("Vale vastus")
+# # Loo programm, mis loob suvalised tehted 1-100 arvudega.
+
+# # Kasuta tsükli puhul alakriipsu
+# # kasuta suvalise tehte märgi jaoks loendit ja sealt suvalise märgi leidmiseks random.choice()
+# # Näiteks:
+# # 7 – 2=
+# # 45 * 69=
+
+# kysimustearv= 5
+# punktid = 0
+# tehted = ['+','-','*','/']
+# for i in range(kysimustearv+1):
+#     i = random.randint(1,10)
+#     j = random.randint(1,10)
+#     tehe = random.choice(tehted)
+#     if tehe=="+":
+#         print(f"{i} {tehe} {j} = ")
+#         vastus = int(input("Vastus: "))
+#         if vastus==i+j:
+#             print("Õige")
+#             punktid+=1
+#         else:
+#             print("Vale vastus")
+#     elif tehe=="-":
+#         print(f"{i} {tehe} {j} = ")
+#         vastus = int(input("Vastus: "))
+#         if vastus==i-j:
+#             print("Õige")
+#             punktid+=1
+#         else:
+#             print("Vale vastus")
+#     elif tehe=="*":
+#         print(f"{i} {tehe} {j} = ")
+#         vastus = int(input("Vastus: "))
+#         if vastus==i*j:
+#             print("Õige")
+#             punktid+=1
+#         else:
+#             print("Vale vastus")
+#     else:
+#         print(f"{i} {tehe} {j} = ")
+#         vastus = float(input("Vastus: "))
+#         if vastus=={round(i/j,2)}:
+#             print("Õige")
+#             punktid+=1
+#         else:
+#             print("Vale vastus")
+
+# # Täienda eelmist ülesannet ja kasutaja käest küsitakse vastust.
+# # Õiged vastused loetakse kokku
+# # # Kui saab vähemalt poole punktid, siis saab A, muul juhul MA
+# try:
+#     if kysimustearv/punktid >= 0.5:
+#         print(punktid, "A")
+#     else:
+#         print(punktid, "MA")
+# except:
+#     print("MA")
+
+
+# Kuva samasugused kujundid:
+
+# for i in range(1,6):
+#     print("*"*i,end =" ")
+#     print()
+        
+# for i in range(1,6):
+#     print("*"*(6-i),end =" ")
+#     print()
+
+# for i in range(1,6):
+#     print(" "*(6-i)+"*"*i,end =" ")
+#     print()
+
+# for i in range(1,6):
+#     print(" "*i+"*"*(6-i),end =" ")
+#     print()
+
+# Paariarvude summa
+
+even_nums = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 3, 32, 34, 36, 38]
+# Kirjuta programm, mis summeerib paarisarve listis even_nums kuni esimese paaritu arvuni.
+
+sum_even = 0
+for i in even_nums:
+    if i%2 == 0:
+        sum_even+=i
     else:
-        print(f"{i} {tehe} {j} = ")
-        vastus = float(input("Vastus: "))
-        if vastus=={round(i/j,2)}:
-            print("Õige")
-            punktid+=1
-        else:
-            print("Vale vastus")
-
-try:
-    if kysimustearv/punktid >= 0.5:
-        print(punktid, "A")
-    else:
-        print(punktid, "MA")
-except:
-    print("MA")
-# Täienda eelmist ülesannet ja kasutaja käest küsitakse vastust.
-# Õiged vastused loetakse kokku
-# # Kui saab vähemalt poole punktid, siis saab A, muul juhul MA
+        break
+        
+print("Tsükkel jõudis lõpuni", sum_even)
 
 
+
+
+# Katkesta tsükkel, kui leitakse paaritu arv, kasutades break.
+# Kui tsükkel lõpetab ilma paaritut arvu kohtamata, prindi sõnum “Tsükkel jõudis lõpuni” ja näita paarisarvude summat.
+# Mitmemõõtmelise massiivi kasutamine for-tsükliga
+# Tutvu elektriautode nimekirjaga, mis sisaldab 10 elektriauto mudelit, nende läbisõidu ulatust ja hinda. Mõista, kuidas andmed on struktureeritud.
 
 
 
