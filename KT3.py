0de394af558fd0fcadd3f8eaa784aa97174097f2
ArtiAasav <arti@hkhk.edu.ee>
2024-12-05 11:36:47 +0200

    Update KT3.py

--- KT3.py
+++ KT3.py
@@ -2,6 +2,7 @@
 # Iseseisev töö
 # Ülesanne 3
 
+# 3.1
 fail = open ("rebased.txt", encoding='UTF-8')
 
 vastuvoetud = []
@@ -15,6 +16,13 @@ a = input("Palun sisetage, millise aasta andmeid vajate?: ")
 
 print(vastuvoetud[int(a[3])-1])
 
+# 3.3
+
+fail = open ("konto.txt", encoding='UTF-8')
+
+for rida in fail:
+    if float(rida) > 0:
+        print(rida,end="")
 
 
 
